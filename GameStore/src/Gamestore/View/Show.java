8a074f927e876bc33a499e0adf484000b5745987
Minsoolee12345 <162321324+Minsoolee12345@users.noreply.github.com>
2024-06-14 17:30:39 +0900

    Add files via upload

---
+++ GameStore/src/Gamestore/View/Show.java
@@ -0,0 +1,35 @@
+package Gamestore.View;
+
+public class Show 
+{
+    public static void welcome() 
+    {
+        String welcome = "*****************************************\n"
+                + "*    Welcome to Minsoo Game Store    *\n"
+                + "*****************************************";
+        System.out.println(welcome);    
+    }
+
+    public void menuList()
+    {
+        System.out.println("=========메뉴 리스트=========");
+        
+        String[] menuList = 
+        {
+                "0. 종료",
+                "1. 게임 정보 보기",
+                "2. 장바구니 보기",
+                "3. 장바구니에 게임 추가",
+                "4. 장바구니 게임 삭제",
+                "5. 장바구니 비우기",
+                "6. 주문하기"
+        };
+        
+        for(String str : menuList) 
+        {
+            System.out.println(str);
+        }
+        
+        System.out.println("===========================");
+    }
+}
